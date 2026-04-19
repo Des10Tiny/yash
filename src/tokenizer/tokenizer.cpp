61e58f8986ad90f7309adb03b9766466e39f8ead
Gleb Makarevich <melanholiya1@icloud.com>
2026-04-19 17:44:20 +0300

    feature(tokenizer): final mvp version for tokenizer
    Add all base sh logic
    Passed all tests
    Need own exeption class in the future

--- src/tokenizer/tokenizer.cpp
+++ src/tokenizer/tokenizer.cpp
@@ -1,26 +1,16 @@
 #include "tokenizer.hpp"
+
 #include <cctype>
 #include <cstdio>
 #include <stdexcept>
 #include <string>
 
-Tokenizer::Tokenizer(std::istream* in) : original_data_(in) {
-    Next();
-}
-
-bool Tokenizer::IsEnd() {
-    return is_end_;
-}
-
-Token Tokenizer::GetToken() {
-    return current_token_;
-}
+namespace {
+enum class State : std::uint8_t { NORMAL, IN_SINGLE, IN_DOUBLE };
 
 bool IsSpecialChar(char c) {
     return c == '|' || c == '>' || c == '<';
 }
-
-enum class State : std::uint8_t { NORMAL, IN_SINGLE, IN_DOUBLE };
 
 std::string ExtractValue(std::istream* data, bool& is_end) {
     std::string result_value;
@@ -29,12 +19,19 @@ std::string ExtractValue(std::istream* data, bool& is_end) {
     while (true) {
         int curr_char = data->get();
 
-        if (state == State::NORMAL) {
-            if (curr_char == EOF) {
-                is_end = true;
-                break;
+        if (curr_char == EOF) {
+            if (state != State::NORMAL) {
+                throw std::runtime_error("Quote not closed");
             }
+            is_end = true;
+            break;
+        }
 
+        if (!std::isprint(curr_char) && !std::isspace(curr_char)) {
+            throw std::runtime_error("Found non-printable symbol");
+        }
+
+        if (state == State::NORMAL) {
             if (std::isspace(static_cast<unsigned char>(curr_char))) {
                 break;
             }
@@ -54,54 +51,41 @@ std::string ExtractValue(std::istream* data, bool& is_end) {
                 continue;
             }
 
-            if (std::isprint(static_cast<unsigned char>(curr_char))) {
-                result_value += curr_char;
-                continue;
-            }
-
-            throw std::runtime_error("Find non ASCII symbol");
+            result_value += curr_char;
         }
 
         else if (state == State::IN_SINGLE) {
-            if (curr_char == EOF) {
-                throw std::runtime_error("Single quote not closed");
-            }
-
             if (curr_char == '\'') {
                 state = State::NORMAL;
-                break;
-            }
-
-            if (std::isprint(static_cast<unsigned char>(curr_char))) {
+            } else {
                 result_value += curr_char;
-                continue;
             }
-
-            throw std::runtime_error("Find non ASCII symbol");
         }
 
         else if (state == State::IN_DOUBLE) {
-            if (curr_char == EOF) {
-                throw std::runtime_error("Double quote not closed");
-            }
 
             if (curr_char == '\"') {
                 state = State::NORMAL;
-                break;
-            }
-
-            if (std::isprint(static_cast<unsigned char>(curr_char))) {
+            } else {
                 result_value += curr_char;
-                continue;
             }
-
-            throw std::runtime_error("Find non ASCII symbol");
         }
-
-        throw std::runtime_error("Find non ASCII symbol");
     }
 
     return result_value;
+}
+}  // namespace
+
+Tokenizer::Tokenizer(std::istream* in) : original_data_(in) {
+    Next();
+}
+
+bool Tokenizer::IsEnd() {
+    return is_end_;
+}
+
+Token Tokenizer::GetToken() {
+    return current_token_;
 }
 
 void Tokenizer::Next() {
@@ -127,39 +111,30 @@ void Tokenizer::Next() {
         }
 
         if (static_cast<char>(current_char) == '>') {
-            int next_char = original_data_->peek();
-
-            if (next_char == '>') {
+            if (original_data_->peek() == '>') {
                 current_token_ = RedirectToken::REDIRECT_APPEND;
                 original_data_->get();
-                return;
+            } else {
+                current_token_ = RedirectToken::REDIRECT_OUT;
             }
 
-            current_token_ = RedirectToken::REDIRECT_OUT;
             return;
         }
 
         if (static_cast<char>(current_char) == '<') {
-            int next_char = original_data_->peek();
-
-            if (next_char == '<') {
+            if (original_data_->peek() == '<') {
                 current_token_ = RedirectToken::HERE_DOC;
                 original_data_->get();
-                return;
+            } else {
+                current_token_ = RedirectToken::REDIRECT_IN;
             }
 
-            current_token_ = RedirectToken::REDIRECT_IN;
             return;
         }
 
-        if (std::isprint(static_cast<unsigned char>(current_char))) {
-            original_data_->unget();
-            std::string extracted_value = ExtractValue(original_data_, is_end_);
-            current_token_ = WordToken{std::move(extracted_value)};
-
-            return;
-        }
-
-        throw std::runtime_error("Find non ASCII symbol");
+        original_data_->unget();
+        std::string extracted_value = ExtractValue(original_data_, is_end_);
+        current_token_ = WordToken{std::move(extracted_value)};
+        return;
     }
 }
