00997ef705d54abb14182130496cfec3346962c5
Gleb Makarevich <melanholiya1@icloud.com>
2026-04-26 20:08:16 +0300

    feat(scoped_fd): add new function and some logger info

--- src/utils/scoped_fd.hpp
+++ src/utils/scoped_fd.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <unistd.h>
+#include <string>
 #include <utility>
 #include <array>
 
@@ -18,6 +19,11 @@ public:
 
         raw_read_fd_ = raw_pipe_fd[0];
         raw_write_fd_ = raw_pipe_fd[1];
+
+        LOG_DEBUG(std::string("Get current FD: Read=") + std::to_string(raw_read_fd_) +
+                  " Write=" + std::to_string(raw_write_fd_)
+
+        );
     }
 
     ScopedFD(int raw_read_fd, int raw_write_fd)
@@ -34,8 +40,7 @@ public:
 
     ScopedFD& operator=(ScopedFD&& other) noexcept {
         if (this != &other) {
-            CloseRawReadFD();
-            CloseRawWriteFD();
+            CloseAllRawFD();
             raw_read_fd_ = std::exchange(other.raw_read_fd_, -1);
             raw_write_fd_ = std::exchange(other.raw_write_fd_, -1);
         }
@@ -45,6 +50,8 @@ public:
 
     void CloseRawReadFD() {
         if (raw_read_fd_ != -1) {
+            is_both_fd_correct_ = false;
+
             if (close(raw_read_fd_) == -1) {
                 throw YashSystemError("Cannot close Read File Descriptor: " +
                                       std::to_string(raw_read_fd_));
@@ -55,23 +62,49 @@ public:
 
     void CloseRawWriteFD() {
         if (raw_write_fd_ != -1) {
+            is_both_fd_correct_ = false;
+
             if (close(raw_write_fd_) == -1) {
                 throw YashSystemError("Cannot close Write File Descriptor: " +
-                                      std::to_string(raw_read_fd_));
+                                      std::to_string(raw_write_fd_));
             }
             raw_write_fd_ = -1;
         }
     }
 
-    int GetRawReadFD() {
+    void CloseAllRawFD() {
+        is_both_fd_correct_ = false;
+
+        if (raw_read_fd_ != -1) {
+            if (close(raw_read_fd_) == -1) {
+                LOG_WARN("Cannot close Read File Descriptor: " + std::to_string(raw_read_fd_));
+            }
+            raw_read_fd_ = -1;
+        }
+
+        if (raw_write_fd_ != -1) {
+            if (close(raw_write_fd_) == -1) {
+                LOG_WARN("Cannot close Write File Descriptor: " + std::to_string(raw_write_fd_));
+            }
+            raw_write_fd_ = -1;
+        }
+    }
+
+    int GetRawReadFD() const {
         return raw_read_fd_;
     }
 
-    int GetRawWriteFD() {
+    int GetRawWriteFD() const {
         return raw_write_fd_;
     }
 
+    bool IsBothCorrect() const {
+        return is_both_fd_correct_;
+    }
+
     ~ScopedFD() {
+        LOG_DEBUG("ScopedFD - call destructor");
+
         if (raw_read_fd_ != -1) {
             if (close(raw_read_fd_) == -1) {
                 LOG_WARN("Cannot close Read File Descriptor: " + std::to_string(raw_read_fd_));
@@ -80,7 +113,7 @@ public:
 
         if (raw_write_fd_ != -1) {
             if (close(raw_write_fd_) == -1) {
-                LOG_WARN("Cannot close Write File Descriptor: " + std::to_string(raw_read_fd_));
+                LOG_WARN("Cannot close Write File Descriptor: " + std::to_string(raw_write_fd_));
             }
         }
     }
@@ -88,4 +121,5 @@ public:
 private:
     int raw_read_fd_ = -1;
     int raw_write_fd_ = -1;
+    bool is_both_fd_correct_ = true;
 };
