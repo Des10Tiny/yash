901cdaa660f3c3581067e7136ea4bafb67c57a8c
Gleb Makarevich <melanholiya1@icloud.com>
2026-04-29 21:42:24 +0300

    test(unique_fd_test): add test for unique_fd

---
+++ tests/unique_fd_test.cpp
@@ -0,0 +1,116 @@
+#include "utils/unique_fd.hpp"
+
+#include <fcntl.h>
+#include <gtest/gtest.h>
+#include <unistd.h>
+
+class UniqueFDTest : public ::testing::Test {
+protected:
+    bool IsDescriptorValid(int fd) {
+        if (fd < 0) {
+            return false;
+        }
+        return fcntl(fd, F_GETFD) != -1;
+    }
+};
+
+TEST_F(UniqueFDTest, ClosesDescriptorOnDestruction) {
+    int raw_fd = open("/dev/null", O_RDONLY);
+    ASSERT_GT(raw_fd, 0);
+    {
+        UniqueFD ufd{raw_fd};
+        EXPECT_EQ(ufd.GetRawFD(), raw_fd);
+        EXPECT_TRUE(IsDescriptorValid(raw_fd));
+    }
+
+    EXPECT_FALSE(IsDescriptorValid(raw_fd));
+    EXPECT_EQ(errno, EBADF);
+}
+
+TEST_F(UniqueFDTest, MoveConstructorTransfersOwnership) {
+    int raw_fd = open("/dev/null", O_RDONLY);
+    UniqueFD ufd1{raw_fd};
+
+    UniqueFD ufd2{std::move(ufd1)};
+
+    EXPECT_EQ(ufd1.GetRawFD(), -1);
+    EXPECT_EQ(ufd2.GetRawFD(), raw_fd);
+    EXPECT_TRUE(IsDescriptorValid(raw_fd));
+}
+
+TEST_F(UniqueFDTest, MoveAssignmentClosesCurrentAndTakesNew) {
+    int fd1 = open("/dev/null", O_RDONLY);
+    int fd2 = open("/dev/null", O_RDONLY);
+
+    {
+        UniqueFD ufd1{fd1};
+        UniqueFD ufd2{fd2};
+
+        ufd2 = std::move(ufd1);
+
+        EXPECT_FALSE(IsDescriptorValid(fd2));
+        EXPECT_EQ(ufd2.GetRawFD(), fd1);
+        EXPECT_EQ(ufd1.GetRawFD(), -1);
+    }
+
+    EXPECT_FALSE(IsDescriptorValid(fd1));
+}
+
+TEST_F(UniqueFDTest, SelfAssignmentDoesNothing) {
+    int raw_fd = open("/dev/null", O_RDONLY);
+    UniqueFD ufd{raw_fd};
+
+    ufd = std::move(ufd);
+
+    EXPECT_EQ(ufd.GetRawFD(), raw_fd);
+    EXPECT_TRUE(IsDescriptorValid(raw_fd));
+}
+
+TEST_F(UniqueFDTest, CreateNewFDPensFileCorrectly) {
+    UniqueFD ufd;
+
+    ASSERT_NO_THROW(ufd.CreateNewFD("/dev/null", O_RDONLY));
+
+    int fd = ufd.GetRawFD();
+    EXPECT_GT(fd, 0);
+    EXPECT_TRUE(IsDescriptorValid(fd));
+}
+
+TEST_F(UniqueFDTest, CreateNewFDClosesPreviousDescriptor) {
+    int first_fd = open("/dev/null", O_RDONLY);
+    UniqueFD ufd{first_fd};
+
+    ufd.CreateNewFD("/dev/null", O_RDONLY);
+
+    EXPECT_FALSE(IsDescriptorValid(first_fd));
+    EXPECT_NE(ufd.GetRawFD(), first_fd);
+}
+
+TEST_F(UniqueFDTest, CreateNewFDThrowsOnInvalidFile) {
+    UniqueFD ufd;
+    EXPECT_THROW(ufd.CreateNewFD("/non_existent_file_12345", O_RDONLY), YashSystemError);
+    EXPECT_EQ(ufd.GetRawFD(), -1);
+}
+
+TEST_F(UniqueFDTest, CloseFDResetsState) {
+    int raw_fd = open("/dev/null", O_RDONLY);
+    UniqueFD ufd{raw_fd};
+
+    ufd.CloseFD();
+
+    EXPECT_EQ(ufd.GetRawFD(), -1);
+    EXPECT_FALSE(IsDescriptorValid(raw_fd));
+}
+
+TEST_F(UniqueFDTest, MultipleMovesWork) {
+    UniqueFD ufd1{open("/dev/null", O_RDONLY)};
+    int raw_fd = ufd1.GetRawFD();
+
+    UniqueFD ufd2 = std::move(ufd1);
+    UniqueFD ufd3 = std::move(ufd2);
+
+    EXPECT_EQ(ufd1.GetRawFD(), -1);
+    EXPECT_EQ(ufd2.GetRawFD(), -1);
+    EXPECT_EQ(ufd3.GetRawFD(), raw_fd);
+    EXPECT_TRUE(IsDescriptorValid(raw_fd));
+}
