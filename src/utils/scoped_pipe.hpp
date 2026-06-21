9c91bef9b06109c0b37040a31e3e4220a76d4c81
Gleb Makarevich <melanholiya1@icloud.com>
2026-06-21 16:56:41 +0300

    ref(unique_fd, scoped_pipe): remove throws
    Remove Yash exptions in throw
    Add pragma_once

--- src/utils/scoped_pipe.hpp
+++ src/utils/scoped_pipe.hpp
@@ -2,21 +2,19 @@
 
 #include <array>
 #include <format>
-#include <string>
 #include <unistd.h>
 #include <utility>
 
 #include "utils/logger.hpp"
-#include "utils/yash_error.hpp"
 
 class ScopedPipe {
 public:
-    explicit ScopedPipe() {
+    explicit ScopedPipe() noexcept {
         raw_read_fd_ = -1;
         raw_write_fd_ = -1;
     };
 
-    ScopedPipe(int raw_read_fd, int raw_write_fd)
+    ScopedPipe(int raw_read_fd, int raw_write_fd) noexcept
         : raw_read_fd_(raw_read_fd)
         , raw_write_fd_(raw_write_fd) {
     }
@@ -39,12 +37,14 @@ public:
         return *this;
     };
 
-    void CreatePipe() {
+    void CreatePipe() noexcept(false) {
         CloseAllRawFD();
         std::array<int, 2> raw_pipe_fd;
 
         if (pipe(raw_pipe_fd.data()) == -1) {
-            throw YashSystemError("CreatePipe(): Cannot make new pipe");
+            throw std::system_error(
+                errno, std::generic_category(), "CreatePipe(): Cannot make new pipe"
+            );
         }
 
         raw_read_fd_ = raw_pipe_fd[0];
@@ -56,64 +56,36 @@ public:
         );
     }
 
-    void CloseRawReadFD() {
-        if (raw_read_fd_ != -1) {
-            if (close(raw_read_fd_) == -1) {
-                throw YashSystemError(
-                    std::format(
-                        "CloseRawReadFD(): Cannot close Read File Descriptor read_fd={}",
-                        raw_read_fd_
-                    )
-                );
-            }
-            raw_read_fd_ = -1;
-        }
+    void CloseRawReadFD() noexcept {
+        CloseSingleFD(
+            raw_read_fd_,
+            "CloseRawReadFD()",
+            std::format("Cannot close Read File Descriptor read_fd={}", raw_read_fd_)
+        );
     }
 
-    void CloseRawWriteFD() {
-        if (raw_write_fd_ != -1) {
-            if (close(raw_write_fd_) == -1) {
-                throw YashSystemError(
-                    std::format(
-                        "CloseRawWriteFD(): Cannot close Write File Descriptor write_fd={}",
-                        raw_write_fd_
-                    )
-                );
-            }
-            raw_write_fd_ = -1;
-        }
+    void CloseRawWriteFD() noexcept {
+        CloseSingleFD(
+            raw_write_fd_,
+            "CloseRawWriteFD()",
+            std::format("Cannot close Write File Descriptor write_fd={}", raw_write_fd_)
+        );
     }
 
-    void CloseAllRawFD() {
-        if (raw_read_fd_ != -1) {
-            if (close(raw_read_fd_) == -1) {
-                LOG_WARN(
-                    "CloseAllRawFD(): Cannot close Read File Descriptor read_fd={}", raw_read_fd_
-
-                );
-            }
-            raw_read_fd_ = -1;
-        }
-
-        if (raw_write_fd_ != -1) {
-            if (close(raw_write_fd_) == -1) {
-                LOG_WARN(
-                    "CloseAllRawFD(): Cannot close Write File Descriptor write_fd={}", raw_write_fd_
-                );
-            }
-            raw_write_fd_ = -1;
-        }
+    void CloseAllRawFD() noexcept {
+        CloseRawReadFD();
+        CloseRawWriteFD();
     }
 
-    [[nodiscard]] int GetRawReadFD() const {
+    [[nodiscard]] int GetRawReadFD() const noexcept {
         return raw_read_fd_;
     }
 
-    [[nodiscard]] int GetRawWriteFD() const {
+    [[nodiscard]] int GetRawWriteFD() const noexcept {
         return raw_write_fd_;
     }
 
-    ~ScopedPipe() {
+    ~ScopedPipe() noexcept {
         LOG_DEBUG("~ScopedFD(): ScopedFD - call destructor");
         CloseAllRawFD();
     }
@@ -121,4 +93,14 @@ public:
 private:
     int raw_read_fd_ = -1;
     int raw_write_fd_ = -1;
+
+    void CloseSingleFD(int& fd, std::string_view label, std::string_view text_for_logger) noexcept {
+        if (fd != -1) {
+            if (close(fd) == -1) {
+                LOG_WARN("{}: {}", label, text_for_logger);
+            }
+
+            fd = -1;
+        }
+    }
 };
